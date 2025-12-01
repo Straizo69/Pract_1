7a302c48ade23f472b414460747aa3dddec15fdb
Evgeniy <117078186+Straizo69@users.noreply.github.com>
2025-12-01 23:26:40 +0200

    Create LR_1_task_2.py

---
+++ LR_1_task_2.py
@@ -0,0 +1,49 @@
+# LR_1_task_2.py
+# Попередня обробка даних: бінаризація, виключення середнього, масштабування, нормалізація
+
+import numpy as np
+from sklearn import preprocessing
+
+# 2.1 – Вхідні дані
+
+input_data = np.array([
+    [-1.3,  3.9,  4.5],
+    [-5.3, -4.2, -1.3],
+    [ 5.2, -6.5, -1.1],
+    [-5.2,  2.6, -2.2]
+])
+
+print("Input data:")
+print(input_data)
+
+# 2.1.1 Бінаризція(3.0)
+
+data_binarized = preprocessing.Binarizer(threshold=3.0).transform(input_data)
+print("\nBinarized data:\n", data_binarized)
+
+# 2.1.2 Викл. середнього
+
+print("\nBEFORE:")
+print("Mean =", input_data.mean(axis=0))
+print("Std deviation =", input_data.std(axis=0))
+
+data_scaled = preprocessing.scale(input_data)
+
+print("\nAFTER:")
+print("Mean =", data_scaled.mean(axis=0))
+print("Std deviation =", data_scaled.std(axis=0))
+
+# 2.1.3 Мастабуання(Min–Max)
+
+data_scaler_minmax = preprocessing.MinMaxScaler(feature_range=(0, 1))
+data_scaled_minmax = data_scaler_minmax.fit_transform(input_data)
+
+print("\nMin–Max scaled data:\n", data_scaled_minmax)
+
+# 2.1.4 Нормалізація(L1 та L2)
+
+data_normalized_l1 = preprocessing.normalize(input_data, norm='l1')
+data_normalized_l2 = preprocessing.normalize(input_data, norm='l2')
+
+print("\nL1 normalized data:\n", data_normalized_l1)
+print("\nL2 normalized data:\n", data_normalized_l2)
